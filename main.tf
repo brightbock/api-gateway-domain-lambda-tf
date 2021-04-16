fce6873129c028a5a10fbf788cd08a081e69bb17
JamesJJ <jj@fcg.fyi>
2021-04-16 14:34:13 +0800

    output more

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ output "api_id" {
   value = aws_api_gateway_rest_api.this.id
 }
 output "api_root_resource_id" {
-  value = aws_api_gateway_rest_api.this.resource_id
+  value = aws_api_gateway_rest_api.this.root_resource_id
 }
 output "api_execution_arn" {
   value = aws_api_gateway_rest_api.this.execution_arn
